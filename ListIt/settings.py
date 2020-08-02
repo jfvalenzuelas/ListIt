8552e662fac026281ab819ad848cce5a013d10b0
Jaime <jaimefvs@gmail.com>
2020-08-02 20:35:05 +0200

    Updated settings.py
    Fixed error for empty value

--- ListIt/settings.py
+++ ListIt/settings.py
@@ -14,7 +14,7 @@ import os
 try:
     from .secrets import *
 except ImportError:
-    SECURITY_KEY = os.getenv('SECRET_KEY', '')
+    SECURITY_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
