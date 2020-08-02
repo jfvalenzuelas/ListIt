a0aadb7e3f3682d63cf2405d3a4eb8335824964a
Jaime <jaimefvs@gmail.com>
2020-08-02 20:32:46 +0200

    Updated settings.py
    SECRET_KEY handling changed

--- ListIt/settings.py
+++ ListIt/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secrets import *
+try:
+    from .secrets import *
+except ImportError:
+    SECURITY_KEY = os.getenv('SECRET_KEY', '')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
