fdcb00a14b1c9d3374e9c8ba97381e793867c860
Jaime <jaimefvs@gmail.com>
2020-08-02 19:49:40 +0200

    Updated setting.py
    SECRET_KEY was transferred to another file

--- ListIt/settings.py
+++ ListIt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm%qmr^co-dgt)uw02iafnw$%59vgva^3716*30pj#y$fv6*$kk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
